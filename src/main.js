40c812e4d2e9d7ecb85261a0eba54dfe3c34139d
Noone <zhinan.zhu@guokr.com>
2018-07-05 10:21:02 +0800

    init

--- src/main.js
+++ src/main.js
@@ -11,12 +11,38 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['pages/logs/main', '^pages/index/main'],
+    pages: ['pages/search/main', 'pages/profile/main', '^pages/board/main'],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
-    }
+    },
+     tabBar: {
+        list: [
+            {
+                text: "榜单",
+                pagePath: "pages/board/main",
+                iconPath: "static/images/board.png",
+                selectedIconPath: "static/images/board-actived.png"
+            },
+            {
+                text: "搜索",
+                pagePath: "pages/search/main",
+                iconPath: "static/images/search.png",
+                selectedIconPath: "static/images/search-actived.png"
+            },
+            {
+                text: "关于我",
+                pagePath: "pages/profile/main",
+                iconPath: "static/images/profile.png",
+                selectedIconPath: "static/images/profile-actived.png"
+            }
+        ],
+        color: "#666666",
+        selectedColor: "#000000",
+        borderStyle: "white",
+        backgroundColor: "#f8f9fb"
+    },
   }
 }
